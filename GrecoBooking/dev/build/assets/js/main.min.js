e419bbbde35162b19d86dd536f7ab809753af96d
enenkoff <enenkoff@mail.ru>
2018-01-28 03:30:49 +0200

    home: search/footer | hovers

--- GrecoBooking/dev/build/assets/js/main.min.js
+++ GrecoBooking/dev/build/assets/js/main.min.js
@@ -1,8 +1,37 @@
 $(document).ready(function () {
 
+    /* ----------------------------------- variables ----------------------------------- */
+
+    var btn = $('#search_btn'),
+        nav = $('.nav'),
+        search = btn.closest('.search');
+
     /* ----------------------------------- functions ----------------------------------- */
 
+    function showSearch() {
+        btn.click(function () {
+            search.addClass('active');
+            nav.addClass('hidden');
+        })
+    }
+
+    function documentClick() {
+        $(document).click(function(e){
+            var targ = $(e.target);
+            if(targ.parents('#searchForm').length == 0){
+                if(targ.parents('#search_btn').length == 0){
+                    search.removeClass('active');
+                    nav.removeClass('hidden');
+                }
+            }
+        })
+
+    }
+
     /* --------------------------------- document load --------------------------------- */
+
+    showSearch();
+    documentClick();
 
     /* --------------------------------- document resize --------------------------------- */
 
