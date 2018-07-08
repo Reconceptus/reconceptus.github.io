81eb409c7bca85d969325711dc8f94270e11b47d
enenkoff <enenkoff@mail.ru>
2018-07-09 02:21:34 +0300

    AP: catalog page

--- Arplans/dev/src/js/partials/scripts.js
+++ Arplans/dev/src/js/partials/scripts.js
@@ -58,6 +58,7 @@ $(document).ready(function () {
                 $thisHideBlock = $thisParent.find('.show-more-hidden');
 
             $this.click(function () {
+                $thisParent.toggleClass('show');
                 $thisHideBlock.slideToggle(300);
             })
 
@@ -121,6 +122,62 @@ $(document).ready(function () {
 
 
     /* ----------------------------------- plugins ----------------------------------- */
+
+    function rangeInput() {
+        var keypressSlider = document.getElementById('keypress'),
+            input0 = document.getElementById('input-with-keypress-0'),
+            input1 = document.getElementById('input-with-keypress-1'),
+            inputs = [input0, input1];
+
+        noUiSlider.create(keypressSlider, {
+            start: [40, 100],
+            step: 10,
+            connect: true,
+            range: {
+                'min': 40,
+                '25%': [100, 10],
+                '65%': [200, 10],
+                'max': 300
+            },
+            format: wNumb({
+                decimals: 0
+            }),
+            pips: {
+                mode: 'range',
+                density: 3
+            }
+        });
+
+        keypressSlider.noUiSlider.on('update', function( values, handle ) {
+            inputs[handle].value = values[handle];
+        });
+
+        function setSliderHandle(i, value) {
+            var r = [null,null];
+            r[i] = value;
+            keypressSlider.noUiSlider.set(r);
+        }
+
+        inputs.forEach(function(input, handle) {
+
+            input.addEventListener('change', function(){
+                setSliderHandle(handle, this.value);
+            });
+
+            input.addEventListener('keydown', function( e ) {
+
+                var values = keypressSlider.noUiSlider.get();
+                var value = Number(values[handle]);
+
+                switch ( e.which ) {
+                    case 13:
+                        setSliderHandle(handle, this.value);
+                        break;
+                }
+            });
+        });
+    }
+
 
     function blogCarousel() {
         var $carouselBox = $('[data-owl="blog"]'),
@@ -195,6 +252,9 @@ $(document).ready(function () {
     }
     if($('.show-more').length > 0){
         showMore();
+    }
+    if($('.range').length > 0){
+        rangeInput();
     }
 
     /* --------------------------------- document resize --------------------------------- */
