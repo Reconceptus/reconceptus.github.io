a6991eb023ec31507a8eab04e2fc0e6daeb23211
enenkov <enenkoff@gmail.com>
2018-08-22 14:40:33 +0300

    AP: range script in the bottom

--- Arplans/dev/src/js/partials/scripts.js
+++ Arplans/dev/src/js/partials/scripts.js
@@ -314,60 +314,7 @@ $(document).ready(function () {
 
     /* ----------------------------------- plugins ----------------------------------- */
 
-    function rangeInput() {
-        var keypressSlider = document.getElementById('keypress'),
-            input0 = document.getElementById('input-with-keypress-0'),
-            input1 = document.getElementById('input-with-keypress-1'),
-            inputs = [input0, input1];
 
-        noUiSlider.create(keypressSlider, {
-            start: [40, 100],
-            step: 10,
-            connect: true,
-            range: {
-                'min': 40,
-                '25%': [100, 10],
-                '65%': [200, 10],
-                'max': 300
-            },
-            format: wNumb({
-                decimals: 0
-            }),
-            pips: {
-                mode: 'range',
-                density: 3
-            }
-        });
-
-        keypressSlider.noUiSlider.on('update', function( values, handle ) {
-            inputs[handle].value = values[handle];
-        });
-
-        function setSliderHandle(i, value) {
-            var r = [null,null];
-            r[i] = value;
-            keypressSlider.noUiSlider.set(r);
-        }
-
-        inputs.forEach(function(input, handle) {
-
-            input.addEventListener('change', function(){
-                setSliderHandle(handle, this.value);
-            });
-
-            input.addEventListener('keydown', function( e ) {
-
-                var values = keypressSlider.noUiSlider.get();
-                var value = Number(values[handle]);
-
-                switch ( e.which ) {
-                    case 13:
-                        setSliderHandle(handle, this.value);
-                        break;
-                }
-            });
-        });
-    }
 
     function blogCarousel() {
         var $carouselBox = $('[data-owl="blog"]'),
@@ -614,9 +561,6 @@ $(document).ready(function () {
     }
     if($('.region-dropbox').length > 0){
         regionDropBox();
-    }
-    if($('.range').length > 0){
-        rangeInput();
     }
     if($('.fixed-scrollbar').length > 0){
         fixedSidebar();
