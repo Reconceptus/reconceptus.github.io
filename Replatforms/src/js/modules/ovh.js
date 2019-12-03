78acd7a9fefaea51d40d77ea5165a94e6176865b
enenkoff <enenkoff@mail.ru>
2019-12-03 03:03:37 +0300

    RP: prevent scroll

--- Replatforms/src/js/modules/ovh.js
+++ Replatforms/src/js/modules/ovh.js
@@ -1,13 +1,31 @@
 let scroll_top;
+
+function disableScroll() {
+    // Get the current page scroll position
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
 const ovh = {
     add: function() {
         scroll_top = $(window).scrollTop();
         setTimeout(function() {
-            $('html').addClass('ovh');
+            // $('html').addClass('ovh');
+            disableScroll();
         }, 300);
     },
     remove: function() {
-        $('html').removeClass('ovh');
+        // $('html').removeClass('ovh');
+        enableScroll();
         $(window).scrollTop(scroll_top);
     },
 };
