276053b3c948302b81f1b9dad4f13f67768ae47c
Aleksandr Enenkov <enenkov@admings-MacBook-Pro.local>
2019-12-10 14:15:52 +0200

    RP: key-benefits field

--- Replatforms/src/js/modules/contenteditable.js
+++ Replatforms/src/js/modules/contenteditable.js
@@ -6,10 +6,14 @@ let contenteditable = {
     data: {
         icons: {
             icon_edit: '<i class="icon icon_size-2"><svg><use xlink:href="#icon-edit"/></svg></i>',
-            icon_approve: '',
-            icon_cancel: '',
-            icon_remove: '',
-            icon_translate: '',
+            icon_approve:
+                '<i class="icon icon_size-2"><svg><use xlink:href="#icon-check"/></svg></i>',
+            icon_cancel:
+                '<i class="icon icon_size-2"><svg><use xlink:href="#icon-close_sm"/></svg></i>',
+            icon_remove:
+                '<i class="icon icon_size-2"><svg><use xlink:href="#icon-remove"/></svg></i>',
+            icon_translate:
+                '<i class="icon icon_size-2"><svg><use xlink:href="#icon-translate"/></svg></i>',
         },
         classes: {
             wrap: 'key_benefits-list--item',
@@ -42,7 +46,9 @@ let contenteditable = {
             el_text = el_parent.find('.contenteditable-item-text'),
             el_textarea = el_parent.find('.contenteditable-item-textarea');
 
-        const currentValue = el_textarea.val();
+        el_textarea.val($.trim(el_textarea.val()));
+
+        el_text.attr('data-value', el_textarea.val());
 
         el_parent.addClass('editing');
         el_textarea.focus();
@@ -52,6 +58,30 @@ let contenteditable = {
             el_text.text(el_textarea.val());
         });
     },
+    approveEditingItem: function(el) {
+        let el_parent = $(el).closest('.contenteditable-item'),
+            el_text = el_parent.find('.contenteditable-item-text');
+
+        el_text.removeAttr('data-value');
+
+        el_parent.removeClass('editing');
+    },
+    cancelEditingItem: function(el) {
+        let el_parent = $(el).closest('.contenteditable-item'),
+            el_text = el_parent.find('.contenteditable-item-text'),
+            el_textarea = el_parent.find('.contenteditable-item-textarea');
+
+        el_text.text(el_text.attr('data-value'));
+        el_textarea.val(el_text.attr('data-value'));
+
+        el_text.removeAttr('data-value');
+
+        el_parent.removeClass('editing');
+    },
+    removeItem: function(el) {
+        let el_parent = $(el).closest('.contenteditable-item');
+        el_parent.remove();
+    },
     insertItem: function(
         translateCallback,
         wrap_class = this.data.classes.wrap,
@@ -60,7 +90,7 @@ let contenteditable = {
         text_class = this.data.classes.text,
         textarea_class = this.data.classes.textarea,
     ) {
-        let _value = c_input.value;
+        let _value = $.trim(c_input.value);
         let itemCode =
             '<div class="contenteditable-item ' +
             wrap_class +
@@ -78,24 +108,30 @@ let contenteditable = {
             '<div class="contenteditable-item-cta ' +
             cta_class +
             '">' +
-            '<button type="button" onclick="functions.contenteditable.editItem(this);" class="' +
+            '<button type="button" onclick="functions.contenteditable.approveEditingItem(this);" class="editing-text-btn ' +
+            btn_class +
+            '">' +
+            this.data.icons.icon_approve +
+            '</button>' +
+            '<button type="button" onclick="functions.contenteditable.cancelEditingItem(this);" class="editing-text-btn ' +
+            btn_class +
+            '">' +
+            this.data.icons.icon_cancel +
+            '</button>' +
+            '<button type="button" onclick="functions.contenteditable.editItem(this);" class="editing-item-btn ' +
             btn_class +
             '">' +
             this.data.icons.icon_edit +
             '</button>' +
-            '<button type="button" class="' +
+            '<button type="button" onclick="functions.contenteditable.removeItem(this);" class="editing-item-btn ' +
             btn_class +
             '">' +
-            '<i class="icon icon_size-2"><svg xmlns="http://www.w3.org/2000/svg">' +
-            '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-remove"/>' +
-            '</svg></i>' +
+            this.data.icons.icon_remove +
             '</button>' +
             '<button type="button" class="' +
             btn_class +
             '">' +
-            '<i class="icon icon_size-2"><svg xmlns="http://www.w3.org/2000/svg">' +
-            '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-translate"/>' +
-            '</svg></i>' +
+            this.data.icons.icon_translate +
             '</button>' +
             '</div>' +
             '</div>';
