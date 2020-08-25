5f6356a93e401be294ac884f72d3f23dd64b21ca
enenkoff <enenkoff@mail.ru>
2020-08-25 18:15:36 +0300

    TS: small fixes

--- Tsvetkova/src/js/modules/expander.js
+++ Tsvetkova/src/js/modules/expander.js
@@ -12,10 +12,10 @@ const expander = {
         const expanders = document.getElementsByClassName(this.data.el.expander);
 
         for (let e = 0; e < expanders.length; e++) {
-            this.expanderHandler(expanders[e]);
+            this.expanderHandler(expanders[e], expanders);
         }
     },
-    expanderHandler(exp) {
+    expanderHandler(exp, all) {
         let toggler = exp.getElementsByClassName(this.data.el.toggle)[0],
             box = exp.getElementsByClassName(this.data.el.box)[0],
             boxInner = exp.getElementsByClassName(this.data.el.boxInner)[0];
@@ -27,7 +27,16 @@ const expander = {
         });
 
         toggler.addEventListener('click', () => {
-            exp.classList.toggle('expanded');
+            if (exp.classList.contains('expanded')) {
+                for (let el = 0; el < all.length; el++) {
+                    all[el].classList.remove('expanded');
+                }
+            } else {
+                for (let el = 0; el < all.length; el++) {
+                    all[el].classList.remove('expanded');
+                }
+                exp.classList.add('expanded');
+            }
         });
     },
 };
